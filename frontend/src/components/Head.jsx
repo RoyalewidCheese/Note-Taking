6d6ef7779bf7eab4710b30356097c884539d9f4b
Jithu Johan <mails4johan@gmail.com>
2025-02-15 12:29:03 +0530

    first commit

---
+++ frontend/src/components/Head.jsx
@@ -0,0 +1,17 @@
+import { Link } from 'react-router-dom';
+
+export default function Head() {
+    const linkStyle = {
+        textDecoration: 'none',
+        color: '#f5400f'
+    }
+  return (
+    <nav>
+        <div className="logo">
+            <Link to={'/'} style={linkStyle}><h2>Note App</h2></Link>
+        </div>
+        <div className="action">
+        </div>
+    </nav>
+  )
+}
